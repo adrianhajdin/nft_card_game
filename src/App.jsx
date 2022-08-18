6c4d789a548ce0d94ed92fd0e01c2bfd287977e0
Tidbits JS <tidbitsjs@gmail.com>
2022-08-18 15:19:11 +0530

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <p className="text-violet-700">
+      Click on the Vite and React logos to learn more
+    </p>
+  </div>
+);
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const home = () => (
+  <div>
+    Home
+  </div>
+);
+
+export default home;
