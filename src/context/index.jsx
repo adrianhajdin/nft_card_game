4ca1f50c1d93ccb14ba18dd6ac0135b604c90a29
Tidbits JS <tidbitsjs@gmail.com>
2022-09-25 23:11:12 +0530

    registerplayer

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
 
 import { abi } from '../abi';
 
@@ -7,33 +8,37 @@ const GlobalContext = createContext();
 
 // // This can be an address or an ENS name
 const address = '0xac1256b9c2c8315716c8d047edd86b13de7ff7c9';
-const provider = ethers.getDefaultProvider('https://api.avax-test.network/ext/bc/C/rpc');
-console.log('Provider', provider);
+const mainprovider = ethers.getDefaultProvider('https://api.avax-test.network/ext/bc/C/rpc');
+console.log('Provider', mainprovider);
 
 export const GlobalContextProvider = ({ children }) => {
   const [battleGround, setBattleGround] = useState('bg-astral');
 
-  // // Read-Only; By connecting to a Provider, allows:
-  // // - Any constant function
-  // // - Querying Filters
-  // // - Populating Unsigned Transactions for non-constant methods
-  // // - Estimating Gas for non-constant (as an anonymous sender)
-  // // - Static Calling non-constant methods (as anonymous sender)
-  const contract = new ethers.Contract(address, abi, provider);
+  const fetchContract = (signerOrProvider) => new ethers.Contract(address, abi, signerOrProvider);
+
   const connectToProvider = async () => {
+    const contract = fetchContract(mainprovider);
     console.log(await contract.FIREBIRD());
   };
 
-  // // Read-Write; By connecting to a Signer, allows:
-  // // - Everything from Read-Only (except as Signer, not anonymous)
-  // // - Sending transactions for non-constant functions
-  // const erc20_rw = new ethers.Contract(address, abi, signer);
+  const registerPlayer = async (playerName) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const contract = fetchContract(signer);
+
+    const newPlayer = await contract.registerPlayer(playerName);
+    console.log('New Player', newPlayer);
+  };
 
   return (
     <GlobalContext.Provider value={{
       battleGround,
       setBattleGround,
       connectToProvider,
+      registerPlayer,
     }}
     >
       {children}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { heroImg, logo } from '../assets';
+import { useGlobalContext } from '../context';
 import styles from '../styles';
 
 const Home = () => {
   const navigate = useNavigate();
+  const { registerPlayer } = useGlobalContext();
+
+  const [playerName, setPlayerName] = useState('');
+
+  const handleClick = () => {
+    if (playerName === '' || playerName.trim() === '') return null;
+
+    navigate('/battleground');
+    registerPlayer(playerName);
+  };
 
   return (
     <div className="min-h-screen flex xl:flex-row flex-col">
@@ -25,9 +36,15 @@ const Home = () => {
           <div className="flex flex-col">
             <div className="flex flex-col">
               <label htmlFor="name" className="font-rajdhani font-semibold text-2xl text-white mb-3">Name</label>
-              <input type="text" placeholder="Enter your battle name" className="bg-siteDimBlack text-white outline-none focus:outline-siteViolet p-4 rounded-md sm:max-w-[50%] max-w-full" />
+              <input
+                type="text"
+                placeholder="Enter your battle name"
+                value={playerName}
+                onChange={(e) => setPlayerName(e.target.value)}
+                className="bg-siteDimBlack text-white outline-none focus:outline-siteViolet p-4 rounded-md sm:max-w-[50%] max-w-full"
+              />
             </div>
-            <button type="button" className="mt-6 px-4 py-2 rounded-lg bg-siteViolet w-fit text-white font-rajdhani font-bold" onClick={() => navigate('/battleground')}>Connect</button>
+            <button type="button" className="mt-6 px-4 py-2 rounded-lg bg-siteViolet w-fit text-white font-rajdhani font-bold" onClick={handleClick}>Connect</button>
           </div>
         </div>
 
