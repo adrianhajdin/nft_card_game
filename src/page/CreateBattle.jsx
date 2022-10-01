d6eec21bd38bf2d3ad8be9b899142b76fea1a940
Tidbits JS <tidbitsjs@gmail.com>
2022-10-01 18:30:30 +0530

    create and join battle page refactor

--- src/page/CreateBattle.jsx
+++ src/page/CreateBattle.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import styles from '../styles';
-import { logo, heroImg } from '../assets';
+import { PageHOC } from '../components';
 import { useGlobalContext } from '../context';
-import { GameLoad } from '../components';
 
 const CreateBattle = () => {
   const { contract, gameData, metamaskAccount } = useGlobalContext();
@@ -40,63 +38,44 @@ const CreateBattle = () => {
   }, [waitBattle, gameData]);
 
   return (
-    <div className="min-h-screen flex xl:flex-row flex-col relative">
-      {waitBattle && <GameLoad />}
-
-      <div className="flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col">
-        <img src={logo} alt="logo" className="w-[160px] h-[52px] object-contain" />
-
-        <div className="flex-1 flex justify-center flex-col xl:mt-0 my-16">
-          <div className="flex flex-row w-full">
-            <h1 className={`flex ${styles.headText} text-left head-text`}>Create <br /> a new Battle</h1>
-          </div>
-
-          <div className="my-10">
-            <p className="font-rajdhani font-normal text-xl text-siteWhite">Create your own battle and wait for other players to join you</p>
-          </div>
-
-          <div className="flex flex-col">
-            <div className="flex flex-col">
-              <label htmlFor="name" className="font-rajdhani font-semibold text-2xl text-white mb-3">Battle</label>
-              <input
-                type="text"
-                placeholder="Enter battle name"
-                disabled={waitBattle}
-                value={battleName}
-                onChange={(e) => setBattleName(e.target.value)}
-                className="bg-siteDimBlack text-white outline-none focus:outline-siteViolet disabled:text-gray-500 p-4 rounded-md sm:max-w-[50%] max-w-full"
-              />
-            </div>
-            <button
-              type="button"
-              className="mt-6 px-4 py-2 rounded-lg bg-siteViolet disabled:bg-gray-500 w-fit text-white font-rajdhani font-bold"
-              disabled={waitBattle}
-              onClick={handleClick}
-            >Create Battle
-            </button>
-          </div>
-
-          {waitBattle ? (
-            <div className="mt-5">
-              <p className="font-rajdhani font-medium text-lg text-siteViolet">Waiting for other player to join...</p>
-            </div>
-          ) : (
-            <p className="font-rajdhani font-medium text-lg text-siteViolet cursor-pointer mt-5"
-              onClick={() => navigate('/join-battle')}
-            >Or join already existing battles
-            </p>
-          )}
-
+    <>
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <label htmlFor="name" className="font-rajdhani font-semibold text-2xl text-white mb-3">Battle</label>
+          <input
+            type="text"
+            placeholder="Enter battle name"
+            disabled={waitBattle}
+            value={battleName}
+            onChange={(e) => setBattleName(e.target.value)}
+            className="bg-siteDimBlack text-white outline-none focus:outline-siteViolet disabled:text-gray-500 p-4 rounded-md sm:max-w-[50%] max-w-full"
+          />
         </div>
-
-        <p className="font-rajdhani font-medium text-base text-white">Made with 💜 by JavaScript Mastery</p>
+        <button
+          type="button"
+          className="mt-6 px-4 py-2 rounded-lg bg-siteViolet disabled:bg-gray-500 w-fit text-white font-rajdhani font-bold"
+          disabled={waitBattle}
+          onClick={handleClick}
+        >Create Battle
+        </button>
       </div>
 
-      <div className="flex flex-1">
-        <img src={heroImg} alt="hero-img" className="w-full xl:h-full" />
-      </div>
-    </div>
+      {waitBattle ? (
+        <div className="mt-5">
+          <p className="font-rajdhani font-medium text-lg text-siteViolet">Waiting for other player to join...</p>
+        </div>
+      ) : (
+        <p className="font-rajdhani font-medium text-lg text-siteViolet cursor-pointer mt-5"
+          onClick={() => navigate('/join-battle')}
+        >Or join already existing battles
+        </p>
+      )}
+    </>
   );
 };
 
-export default CreateBattle;
+export default PageHOC(
+  CreateBattle,
+  <>Create <br /> a new Battle</>,
+  <>Create your own battle and wait for other players to join you</>,
+);
--- src/page/JoinBattle.jsx
+++ src/page/JoinBattle.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import styles from '../styles';
-import { logo, heroImg } from '../assets';
+import { PageHOC } from '../components';
 import { useGlobalContext } from '../context';
 
 const JoinBattle = () => {
@@ -28,54 +27,37 @@ const JoinBattle = () => {
   };
 
   return (
-    <div className="min-h-screen flex xl:flex-row flex-col">
-      <div className="flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col">
-        <img src={logo} alt="logo" className="w-[160px] h-[52px] object-contain" />
+    <>
+      <p className="font-rajdhani font-semibold text-2xl text-white mb-3">Available Battles:</p>
 
-        <div className="flex-1 flex justify-center flex-col my-16">
-          <div className="flex flex-row w-full">
-            <div className="w-1 h-[120px] bg-siteViolet mr-3" />
-            <h1 className={`${styles.headText} text-left`}>Join <br /> a Battle</h1>
+      <div className="flex flex-col gap-3">
+        {gameData.pendingBattles.length ? gameData.pendingBattles.filter((battle) => battle.battleStatus !== 1).map((battle, index) => (
+          <div key={battle.name + index} className="flex justify-between items-center">
+            <p className="font-rajdhani font-normal text-xl text-white">{index + 1}. {battle.name}</p>
+            <button
+              type="button"
+              className="px-4 py-2 rounded-lg bg-siteViolet w-fit text-white font-rajdhani font-bold"
+              onClick={() => handleClick(battle.name)}
+            >Join
+            </button>
           </div>
-
-          <div className="my-10">
-            <p className="font-rajdhani font-normal text-xl text-siteWhite">Join already existing battles</p>
-          </div>
-
-          <p className="font-rajdhani font-semibold text-2xl text-white mb-3">Available Battles:</p>
-
-          <div className="flex flex-col gap-3">
-            {gameData.pendingBattles.length ? gameData.pendingBattles.filter((battle) => battle.battleStatus !== 1).map((battle, index) => (
-              <div key={battle.name + index} className="flex justify-between items-center">
-                <p className="font-rajdhani font-normal text-xl text-white">{index + 1}. {battle.name}</p>
-                <button
-                  type="button"
-                  className="px-4 py-2 rounded-lg bg-siteViolet w-fit text-white font-rajdhani font-bold"
-                  onClick={() => handleClick(battle.name)}
-                >Join
-                </button>
-              </div>
-            )) : (
-              <p className="font-rajdhani font-normal text-xl text-white">Loading...</p>
-            )}
-          </div>
-
-          {errorMessage && <p className="text-red-500 text-xl">{errorMessage}</p>}
-
-          <p className="font-rajdhani font-medium text-lg text-siteViolet cursor-pointer mt-5"
-            onClick={() => navigate('/create-battle')}
-          >Or create a new battle
-          </p>
-        </div>
-
-        <p className="font-rajdhani font-medium text-base text-white">Made with 💜 by JavaScript Mastery</p>
+        )) : (
+          <p className="font-rajdhani font-normal text-xl text-white">Loading...</p>
+        )}
       </div>
 
-      <div className="flex flex-1">
-        <img src={heroImg} alt="hero-img" className="w-full xl:h-full" />
-      </div>
-    </div>
+      {errorMessage && <p className="text-red-500 text-xl">{errorMessage}</p>}
+
+      <p className="font-rajdhani font-medium text-lg text-siteViolet cursor-pointer mt-5"
+        onClick={() => navigate('/create-battle')}
+      >Or create a new battle
+      </p>
+    </>
   );
 };
 
-export default JoinBattle;
+export default PageHOC(
+  JoinBattle,
+  <>Join <br /> a Battle</>,
+  <>Join already existing battles</>,
+);
