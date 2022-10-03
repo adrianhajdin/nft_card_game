8fcd71b7637d22fced7419fa615a0006487f9c8d
Tidbits JS <tidbitsjs@gmail.com>
2022-10-04 00:17:06 +0530

    card tilt effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { randomCardGenerator } from '../data/cards';
+import Tilt from 'react-parallax-tilt';
 
 import styles from '../styles';
+import { randomCardGenerator } from '../data/cards';
 
 const img1 = randomCardGenerator().img;
 const img2 = randomCardGenerator().img;
 
 const Card = ({ card, title, restStyles, playerTwo }) => (
-  <div className={`${styles.cardContainer} ${restStyles} transition-all`}>
-    <img src={playerTwo ? img2 : img1} alt="ace_card" className={styles.cardImg} />
+  <Tilt>
+    <div className={`${styles.cardContainer} ${restStyles} transition-all`}>
+      <img src={playerTwo ? img2 : img1} alt="ace_card" className={styles.cardImg} />
 
-    <div className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}>
-      <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
-    </div>
-    <div className={`${styles.cardPointContainer} sm:right-[14.2%] right-[15%] ${styles.flexCenter}`}>
-      <p className={`${styles.cardPoint} text-red-700`}>{card.def}</p>
-    </div>
+      <div className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}>
+        <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
+      </div>
+      <div className={`${styles.cardPointContainer} sm:right-[14.2%] right-[15%] ${styles.flexCenter}`}>
+        <p className={`${styles.cardPoint} text-red-700`}>{card.def}</p>
+      </div>
 
-    <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
-      <p className={styles.cardText}>{title}</p>
+      <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
+        <p className={styles.cardText}>{title}</p>
+      </div>
     </div>
-  </div>
+  </Tilt>
 );
 
 export default Card;
