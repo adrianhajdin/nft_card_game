bd2dfd783265b29340ff2e75479d2d536a7c3a4e
Tidbits JS <tidbitsjs@gmail.com>
2022-10-09 00:22:16 +0530

    hoc page styles

--- src/components/PageHOC.jsx
+++ src/components/PageHOC.jsx
@@ -11,25 +11,23 @@ const PageHOC = (Component, title, description) => () => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen flex xl:flex-row flex-col relative">
+    <div className={styles.hocContainer}>
       {showAlert?.status && <Alert type={showAlert.type} message={showAlert.message} />}
 
-      <div className="flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col">
-        <img src={logo} alt="logo" className="w-[160px] h-[52px] object-contain" onClick={() => navigate('/')} />
+      <div className={styles.hocContentBox}>
+        <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
 
-        <div className="flex-1 flex justify-center flex-col xl:mt-0 my-16">
+        <div className={styles.hocBodyWrapper}>
           <div className="flex flex-row w-full">
-            <h1 className={`flex ${styles.headText} text-left head-text`}>{title}</h1>
+            <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
           </div>
 
-          <div className="my-10">
-            <p className="font-rajdhani font-normal text-[24px] text-siteWhite">{description}</p>
-          </div>
+          <p className={`${styles.normalText} my-10`}>{description}</p>
 
           <Component />
         </div>
 
-        <p className="font-rajdhani font-medium text-base text-white">Made with 💜 by JavaScript Mastery</p>
+        <p className={styles.footerText}>Made with 💜 by JavaScript Mastery</p>
       </div>
 
       <div className="flex flex-1">
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,9 +1,17 @@
 const styles = {
   // general
   headText: 'font-rajdhani font-bold text-white sm:text-6xl text-4xl',
+  normalText: 'font-rajdhani font-normal text-[24px] text-siteWhite',
+  footerText: 'font-rajdhani font-medium text-base text-white',
 
   // glassmorphism
   glassEffect: 'bg-white backdrop-filter backdrop-blur-lg bg-opacity-10',
+
+  // hoc page
+  hocContainer: 'min-h-screen flex xl:flex-row flex-col relative',
+  hocContentBox: 'flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col',
+  hocLogo: 'w-[160px] h-[52px] object-contain',
+  hocBodyWrapper: 'flex-1 flex justify-center flex-col xl:mt-0 my-16',
 
   // Game page
   gameContainer: 'w-screen min-h-screen',
