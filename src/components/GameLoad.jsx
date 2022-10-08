35eb9eba5795de3cc7963dbe56e08bcef312468f
Tidbits JS <tidbitsjs@gmail.com>
2022-10-08 19:57:34 +0530

    battleground page linking

--- src/components/GameLoad.jsx
+++ src/components/GameLoad.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styles from '../styles';
 import { player01, player02 } from '../assets';
 import { useGlobalContext } from '../context';
 
 const GameLoad = () => {
+  const navigate = useNavigate();
   const { metamaskAccount } = useGlobalContext();
 
   return (
-    <div className={`absolute z-10 w-full h-full ${styles.flexCenter} gameload flex-col`}>
-      <h1 className="font-bold font-rajdhani text-white text-6xl text-center">Waiting for a worthy opponent...</h1>
-      <p className="font-rajdhani text-siteWhite text-2xl mt-5 text-center">Protip: while you're waiting, choose your preferred battleground</p>
+    <div className="absolute inset-0 z-10 w-full h-full flex justify-between items-center gameload flex-col">
+      <div className="w-full flex justify-end px-8">
+        <button
+          type="button"
+          className="mt-6 px-4 py-2 rounded-lg bg-siteViolet disabled:bg-gray-500 w-fit text-white font-rajdhani font-bold"
+          onClick={() => navigate('/battleground')}
+        >
+          Choose Battleground
+        </button>
+      </div>
 
-      <div className="flex justify-evenly items-center mt-20">
-        <div className={`${styles.flexCenter} flex-col`}>
-          <img src={player01} className="w-36 h-36 object-contain rounded-full drop-shadow-lg" />
-          <p className="mt-3 font-rajdhani text-white text-xl">{metamaskAccount.slice(0, 30)}</p>
-        </div>
+      <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+        <h1 className="font-bold font-rajdhani text-white text-6xl text-center">Waiting for a worthy opponent...</h1>
+        <p className="font-rajdhani text-siteWhite text-2xl mt-5 text-center">Protip: while you're waiting, choose your preferred battleground</p>
 
-        <h2 className="font-rajdhani font-extrabold text-siteViolet text-7xl mx-16">
-          <span className="text-8xl">VS</span>
-        </h2>
+        <div className="flex justify-evenly items-center mt-20">
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img src={player01} className="w-36 h-36 object-contain rounded-full drop-shadow-lg" />
+            <p className="mt-3 font-rajdhani text-white text-xl">{metamaskAccount.slice(0, 30)}</p>
+          </div>
 
-        <div className={`${styles.flexCenter} flex-col`}>
-          <img src={player02} className="w-36 h-36 object-contain rounded-full drop-shadow-lg" />
-          <p className="mt-3 font-rajdhani text-white text-xl">???</p>
+          <h2 className="font-rajdhani font-extrabold text-siteViolet text-7xl mx-16">
+            <span className="text-8xl">VS</span>
+          </h2>
+
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img src={player02} className="w-36 h-36 object-contain rounded-full drop-shadow-lg" />
+            <p className="mt-3 font-rajdhani text-white text-xl">???</p>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };
--- src/page/Battleground.jsx
+++ src/page/Battleground.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import styles from '../styles';
+import { Alert } from '../components';
 import { battlegrounds } from '../assets';
 import { useGlobalContext } from '../context';
-import styles from '../styles';
 
 const Battleground = () => {
-  const { setBattleGround } = useGlobalContext();
+  const navigate = useNavigate();
+  const { setBattleGround, setShowAlert, showAlert } = useGlobalContext();
+
+  const handleBattleChoice = (ground) => {
+    setBattleGround(ground.id);
+    setShowAlert({ status: true, type: 'info', message: `${ground.name} is battle ready!` });
+
+    setTimeout(() => {
+      navigate(-1);
+    }, 1000);
+  };
 
   return (
     <div className={`${styles.flexCenter} min-h-screen bg-landing flex-col py-12 px-4`}>
+      {showAlert.status && <Alert type={showAlert.type} message={showAlert.message} />}
+
       <h1 className={`${styles.headText} text-center`}>Choose your <span className="text-siteViolet">Battle</span> Ground</h1>
 
       <div className={`${styles.flexCenter} flex-wrap mt-10 max-w-[1200px]`}>
         {battlegrounds.map((ground) => (
-          <div key={ground.id} className={`${styles.flexCenter} sm:w-[420px] w-full h-[260px] p-2 glass-morphism m-4 rounded-lg cursor-pointer battle-card`} onClick={() => setBattleGround(ground.id)}>
+          <div key={ground.id} className={`${styles.flexCenter} sm:w-[420px] w-full h-[260px] p-2 glass-morphism m-4 rounded-lg cursor-pointer battle-card`} onClick={() => handleBattleChoice(ground)}>
             <img src={ground.image} alt="saiman" className="w-full h-full object-cover rounded-md" />
 
             <div className="info absolute">
