b0a0b293cc0f357e66a79eb2f16c83eb4bd4596e
adrianhajdin <adrianhajdin00@gmail.com>
2022-10-12 14:16:25 +0200

    ...

--- src/contract/index.js
+++ src/contract/index.js
@@ -1,4 +1,4 @@
 import contract from './AVAXGods.json';
 
-export const ADDRESS = '0xA96c383932DEaC92e91190e58Fd3bbA51b2C066E';
+export const ADDRESS = '0xC8A3083367ef44ed9cbF260c97a7aD6a3d951BF7';
 export const { abi: ABI } = contract;
